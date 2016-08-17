97ab200a46abddb489fbc6ae5a42169035c8a53a
ltimar <luci@rimroe.elster-group.com>
2016-08-17 18:07:54 +0300

    aaa

--- WebApplicationWebDB/Models/LoriVideo.cs
+++ WebApplicationWebDB/Models/LoriVideo.cs
@@ -7,6 +7,7 @@ namespace WebApplicationWebDB.Models
 {
     public class LoriVideo
     {
+		//
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
